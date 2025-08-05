54d2767e62b8f1c2b9a273545c906956c295bdbf
DEEPAK PAREEK <59772415+DeepakPareek-debug@users.noreply.github.com>
2025-08-05 14:39:15 +0000

    private object (Symbol)

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,12 @@
 // Write your code here
+const username = Symbol("username");
+const password = Symbol("password");
+
+const user = {
+  [username]:  "ABCD",
+  [password]: "123456789",
+  age: 20
+};
+
+console.log(user.username);
+console.log(user.password);
