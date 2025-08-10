17d79b3362b4ffa22344a90985b8893daa57b207
DEEPAK PAREEK <59772415+DeepakPareek-debug@users.noreply.github.com>
2025-08-10 17:58:09 +0000

    time, class, and colors

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,18 @@
 // Write your code here
+function getStudents(classroom){
+  let {hasTeachingAssistant, classList} = classroom;
+  let teacher,teachingAssistant,Students;
+
+  if(hasTeachingAssistant){
+    [teacher,teachingAssistant,...Students] = classList;
+  }else{
+    [teacher,...Students] = classList;
+  }
+
+  return Students
+}
+
+console.log(getStudents({
+  hasTeachingAssistant: true,
+  classList: ["A","B","C","D","E"]
+}));
